8a920e59c8e5cb6428f094b50089309ab7084b2b
AingeruRBlol <83420475+AingeruRBlol@users.noreply.github.com>
2024-01-09 11:41:27 +0100

    Delete ISAD23_biblioteca-main/model/tools.py

--- ISAD23_biblioteca-main/model/tools.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-salt = "library"
-
-def hash_password(passw):
-	dataBase_password = passw + salt
-	hashed = hashlib.md5(dataBase_password.encode())
-	dataBase_password = hashed.hexdigest()
-	return dataBase_password
